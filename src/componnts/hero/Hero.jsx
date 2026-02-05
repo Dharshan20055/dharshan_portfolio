6513456779de128e83a72e13636b16d0ed4e0db4
Dharshan20055 <dharshanmurali05@gmail.com>
2026-02-05 16:16:52 +0530

    Changed Name in Home

--- src/componnts/hero/Hero.jsx
+++ src/componnts/hero/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
         </div>
         <div className="left">
           <h2>Hey</h2>
-          <h1>I am Dharshan</h1>
+          <h1>I am Dharsuun</h1>
           <h3>Computer Science <span>Engineer</span></h3>
           <p>I am Dharshan M, an ambitious engineering student skilled in programming and web development. I have worked on innovative projects like an Alumni App and Portfolio Website, showcasing my creativity and technical expertise.</p>
           <div className="social-links-hero">
